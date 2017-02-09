fbffe8a874ee677ed31b0d4b67504006c90e32b0
Daniel Kramer <drkspace@gmail.com>
2017-02-09 11:25:33 -0500

    added support for RTIMU

--- positioning.py
+++ positioning.py
@@ -1,15 +1,39 @@
 import sys
-#Need to update this based on the pi
-#http://github.com/mwilliams03/BerryIMU.git <- need to clone this to get the import
-sys.path.insert(0, '/home/daniel/pythonstuff/Steamworks/BerryIMU/python-BerryIMU-gryo-accel-compass')
-from berryIMU import *
+import getopt
 from Decimal import *
 import math
+import time
+
+sys.path.append('/home/pi/Downloads/RTIMULib/Linux/python')
+import RTIMU
+
+SETTINGS_FILE = "/home/pi/Downloads/RTIMULib/Linux/python/RTIMULib"
+
+print("Using settings file " + SETTINGS_FILE + ".ini")
+if not os.path.exists(SETTINGS_FILE + ".ini"):
+  print("Settings file does not exist, will be created")
+
+s = RTIMU.Settings(SETTINGS_FILE)
+
+imu = RTIMU.RTIMU(s)
+
+if (not imu.IMUInit()):
+    print("IMU Init Failed")
+    sys.exit(1)
+else:
+    print("IMU Init Succeeded")
+
+imu.setSlerpPower(0.02)
+imu.setGyroEnable(True)
+imu.setAccelEnable(True)
+
+
+poll_interval = imu.IMUGetPollInterval()
 
 getcontext().prec = 10
 
-refresh_rate = decimal('1') #in kHz
-time_between = decima1('1')/(refresh_rate*decimal('1000'))
+refresh_rate = decimal(poll_interval)
+time_between = refresh_rate/decimal('1000')
 half_time_squared = (time_between**decima1('2'))*decimal('.5')
 
 file = ''
@@ -25,13 +49,17 @@ x_pos, y_pos = getStartingPostion()
 
 #Method to return the angle of the gyro
 def getTheta():
-    return readGYRz()
-    
+    data= imu.getIMUData()
+    rpy=data["fusionPose"]
+    return math.degrees(rpy[2])
+
 #Method to return the acceleration form the accelerometer 
 def getAccelerations():
-    x_accel_tmp = readACCx()
-    y_accel_tmp = readACCy()
-    theta = getTheta
+    x,y,_ = imu.getFusionData()
+    
+    x_accel_tmp = x
+    y_accel_tmp = y
+    theta = getTheta()
     x_accel = math.sin(theta)*y_accel_tmp+math.cos(theta)*x_accel_tmp
     y_accel = math.sin(theta)*x_accel_tmp+math.cos(theta)*y_accel_tmp
     ##z_accel = 0.0
@@ -47,22 +75,11 @@ def calulatePosition():
     x_pos+=x_offset
     y_pos+=y_offset
     
-def setUp():
-    global file
-    file = '/dev/i2c-%d'
-    file = open(filename, O_RDWR)
-    if file < 0:
-        print('Unable to open I2C bus! tyring again')
-        return False
-    else:
-        return true
 
 def main():
-    while setUp()==False:
-        pass
     while(True):
-        calculatePosition
-        
+        calculatePosition()
+        time.sleep(float(time_between))
     
 
     
