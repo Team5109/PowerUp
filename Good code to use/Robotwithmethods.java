a248f6063a990de3e6121a2b24e1620c5daf9dda
Jushwee <35314294+Jushwee@users.noreply.github.com>
2018-09-19 17:20:44 -0400

    Update Robotwithmethods.java

--- Good code to use/Robotwithmethods.java
+++ Good code to use/Robotwithmethods.java
@@ -27,15 +27,15 @@ import edu.wpi.first.wpilibj.DriverStation;
  * directory.
  */
 public class Robot extends IterativeRobot {
-    TalonSRX leftMotor1 =  new TalonSRX(8);
-    TalonSRX leftMotor2 =  new TalonSRX(9);
-    TalonSRX rightMotor1 =  new TalonSRX(7);//10
-    TalonSRX rightMotor2 =  new TalonSRX(5);//10
+    TalonSRX leftMotor1 =  new TalonSRX(6); //6, 10, 5, 4
+    TalonSRX leftMotor2 =  new TalonSRX(10);
+    TalonSRX rightMotor1 =  new TalonSRX(5);//10
+    TalonSRX rightMotor2 =  new TalonSRX(4);//10
     TalonSRX scalar = new TalonSRX(0);
     Compressor compressor;
     boolean lowgear = false;
-    Encoder rightEncoder = new Encoder(0, 1, true);
-    Encoder leftEncoder = new Encoder(8, 9, false);
+    Encoder rightEncoder = new Encoder(0, 1, true); //true
+    Encoder leftEncoder = new Encoder(8, 9, false); //false
     double  leftspeed = 0;
 	double rightspeed = 0;
 	long idealright = 0;
@@ -61,10 +61,20 @@ public class Robot extends IterativeRobot {
     
     @Override
     public void autonomousInit() {
+    	leftEncoder.reset();
+    	rightEncoder.reset();
    	    }  
     public void autonomousPeriodic() {
-   	 
-   	 
+   	 if(Counter == 0) {
+   		 driveStraight(5, 160);
+   	 }
+   	 else {
+   		Timer.delay(1);
+   		leftTurn(95);
+   	 }
+   	 System.out.println("right " + rightEncoder.get());
+   	 System.out.println("left " + leftEncoder.get());
+   	 System.out.println(Counter);
     }
   	 
     
@@ -80,16 +90,20 @@ public class Robot extends IterativeRobot {
     
     @Override
     public void testInit() {    
+    	leftEncoder.reset();
+    	rightEncoder.reset();
     }
 
 
     public void testPeriodic() {
+    	System.out.println("left: " + leftEncoder.get());
+		 System.out.println("right: " + rightEncoder.get());
    	 
     }
 
 
 
-public void driveStraight(double distance) {   //distance should be in feet
+public void driveStraight(double distance, int speed) {   //distance should be in feet, speed should be in encoder counts per cycle (40 is our base)
 	double encoder_counts = distance * 1564.556753;
 	double Acceleration = 0.03;
 	long leftCount = leftEncoder.get();
@@ -99,27 +113,27 @@ public void driveStraight(double distance) {   //distance should be in feet
 	idealleft = leftEncoder.get();
 	idealright = rightEncoder.get();
 		if(leftCount <= encoder_counts && rightCount < encoder_counts) {	
-			if (leftChange == 40) {
+			if (leftChange == speed) {
 			}
-			else if (leftChange >= 40) {
+			else if (leftChange >= speed) {
 				leftspeed = leftspeed - Acceleration;
 			}
-			else if (leftChange <= 40) {
+			else if (leftChange <= speed) {
 				leftspeed = leftspeed + Acceleration;	
 			}
-			if (rightChange == 40) {
+			if (rightChange == speed) {
 			}
-			else if (rightChange >= 40) {
+			else if (rightChange >= speed) {
 				rightspeed = rightspeed - Acceleration;
 			}
-			else if (rightChange <= 40) {
+			else if (rightChange <= speed) {
 				rightspeed = rightspeed + Acceleration;	
 			}
-			if (leftspeed >= 0.5) {
-				leftspeed = 0.5;
+			if (leftspeed >= 0.7) {
+				leftspeed = 0.7;
 			}
-			if (rightspeed >= 0.5) {
-				rightspeed = 0.5;
+			if (rightspeed >= 0.7) {
+				rightspeed = 0.7;
 			}
 			leftMotor1.set(ControlMode.PercentOutput, -leftspeed);
 			leftMotor2.set(ControlMode.PercentOutput, -leftspeed);
@@ -134,7 +148,48 @@ public void driveStraight(double distance) {   //distance should be in feet
 			rightMotor2.set(ControlMode.PercentOutput, 0);
 			leftEncoder.reset();
 			rightEncoder.reset();
+			Counter ++;
 		}
 	
 	  }
+public void leftTurn(double degrees) { //degrees should be in degrees, basically out of 360
+	long leftCount = leftEncoder.get();
+	long rightCount = rightEncoder.get();
+	double distanceR = 34.4444444*degrees;
+	double distanceL = -18.888888*degrees;
+	if(rightCount <= distanceR || leftCount>= distanceL) {
+		leftMotor1.set(ControlMode.PercentOutput, 0.35);
+		leftMotor2.set(ControlMode.PercentOutput, 0.35);
+		rightMotor1.set(ControlMode.PercentOutput, 0.35);
+		rightMotor2.set(ControlMode.PercentOutput, 0.35);
+	}
+	else {
+	leftMotor1.set(ControlMode.PercentOutput, 0);
+	leftMotor2.set(ControlMode.PercentOutput, 0);
+	rightMotor1.set(ControlMode.PercentOutput, 0);
+	rightMotor2.set(ControlMode.PercentOutput, 0);
+
+		
+	}
+}
+public void rightTurn(double degrees) { //degrees should be in degrees, basically out of 360
+	long leftCount = leftEncoder.get();
+	long rightCount = rightEncoder.get();
+	double distanceL = 18.889*degrees;
+	double distanceR = -18.889*degrees;
+	if(leftCount <= distanceL || rightCount>= distanceR) {
+		leftMotor1.set(ControlMode.PercentOutput, -0.35);
+		leftMotor2.set(ControlMode.PercentOutput, -0.35);
+		rightMotor1.set(ControlMode.PercentOutput, -0.35);
+		rightMotor2.set(ControlMode.PercentOutput, -0.35);
+	}
+	else {
+	leftMotor1.set(ControlMode.PercentOutput, 0);
+	leftMotor2.set(ControlMode.PercentOutput, 0);
+	rightMotor1.set(ControlMode.PercentOutput, 0);
+	rightMotor2.set(ControlMode.PercentOutput, 0);
+
+		
+	}
+}
 }
